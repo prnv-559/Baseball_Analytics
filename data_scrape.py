77d22a6b68aa5d65369596c87d6f3a79fc0892ba
kylekogami <60519319+kylekogami@users.noreply.github.com>
2020-11-08 16:11:40 -0800

    Add files via upload
    updated to take data from 2015-2019

--- data_scrape.py
+++ data_scrape.py
@@ -5,22 +5,30 @@ import os
 import csv
 
 #batters
-batter_data = batting_stats(2018, 2019, qual=502)
+batter_data = batting_stats(2015, 2019, qual = 100)
 batter_data.head(0).to_csv("batters.csv", index = False, sep=',', encoding='utf-8')
 
+df = pd.DataFrame(batter_data)
+df.sort_values(by=["Season"], inplace=True)
+print(df)
+
+
 counter = {}
-for b in batter_data.IDfg:
+for b in df.IDfg:
     counter[b] = counter.get(b, 0) + 1
-idfg = ([key for key, count in counter.items() if count == 2])
+idfg = ([key for key, count in counter.items() if count == 5])
 print(idfg)
 
-batter_data.set_index("IDfg", inplace=True)
-batter_data.head(1000)
-for batters in idfg:
-    print(batter_data.loc[batters])
-    with open("batters.csv", 'a') as f:
-        batter_data.loc[batters].to_csv(f, header=False)
+df.set_index("IDfg", inplace=True)
+df.head(1000)
+for id in idfg:
+    print(df.loc[id])
+    #batter_data.set_index("Season", inplace=True)
+    with open("batters.csv", 'a+') as f:
+        df.loc[id].to_csv(f, header=False)
 
+
+'''
 #pitchers
 pitcher_data = pitching_stats(2018, 2019, qual=162)
 pitcher_data.head(0).to_csv("pitchers.csv", index = False, sep=',', encoding='utf-8')
@@ -37,3 +45,4 @@ for pitchers in idfg:
     print(pitcher_data.loc[pitchers])
     with open("pitchers.csv", 'a') as f:
         pitcher_data.loc[pitchers].to_csv(f, header=False)
+'''
